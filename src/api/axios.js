3962dd8aee1f109daad64a27567c577d5cce21ac
michael777-3333 <131264034+michael777-3333@users.noreply.github.com>
2024-02-01 12:38:29 -0500

    change de render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'https://pelismike.onrender.com/api',
+    // baseURL:'https://pelismike.onrender.com/api',
     // baseURL:'http://localhost:3000/api',
+    baseURL:'https://pelis-q6zx.onrender.com',
     withCredentials:true
 })
 
