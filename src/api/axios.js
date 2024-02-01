a6193d3e5c033b6269d8035e6a784d12465d8a49
michael777-3333 <131264034+michael777-3333@users.noreply.github.com>
2024-02-01 12:45:21 -0500

    add api

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     // baseURL:'https://pelismike.onrender.com/api',
     // baseURL:'http://localhost:3000/api',
-    baseURL:'https://pelis-q6zx.onrender.com',
+    baseURL:'https://pelis-q6zx.onrender.com/api',
     withCredentials:true
 })
 
